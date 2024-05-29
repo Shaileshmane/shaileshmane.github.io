952126cca079816ff86ca02809bf908ca2a571d7
Shaileshmane <105961316+Shaileshmane@users.noreply.github.com>
2024-05-30 01:30:18 +0530

    mail feature added

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.getElementById('toggleButton');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const toggleButton = document.getElementById('toggleButton');
     
-    toggleButton.addEventListener('click', function() {
-        document.body.classList.toggle('dark-mode')
+//     toggleButton.addEventListener('click', function() {
+//         document.body.classList.toggle('dark-mode')
     
-    });
-});
+//     });
+// });
+
+
+
+function SendMail(){
+    
+var templateParams = {
+    name: document.getElementById("fullname").value,
+    email : document.getElementById("email").value,
+    message: document.getElementById("message").value
+  };
+  
+  emailjs.send('service_vo8ka9i', 'template_6z6xe1y', templateParams).then(
+    (response) => {
+      console.log('SUCCESS!', response.status, response.text);
+    },
+    (error) => {
+      console.log('FAILED...', error);
+    },
+  );
+
+}
